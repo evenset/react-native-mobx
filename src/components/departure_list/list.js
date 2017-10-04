046f9564b4d2fbec8363cea52729fb613e071a27
David Walker <davidw@evenset.com>
2017-10-04 15:41:38 -0400

    initial commit

--- src/components/departure_list/list.js
+++ src/components/departure_list/list.js
@@ -1,32 +1,33 @@
 // @flow
 
 import React from 'react';
-import { Button, SectionList, View } from 'react-native';
-
+import { Button, SectionList, View, Text } from 'react-native';
+import { observer } from 'mobx-react';
 import Departure from './departure';
 import Header from './header';
 import styles from '../../styles';
+import { store } from '../../store/schedule.js';
 
 
 // NOTE: this is temporary data until MobX is hooked up
-const SECTIONS = [
-    {
-        data: [
-            { key: 'something', time: '12:34', name: 'Union 12:34 - Georgetown 13:33' },
-            { key: 'somethign', time: '12:34', name: 'Union 12:34 - Georgetown 13:33' },
-        ],
-        routeColor: '#0000BB',
-        title: 'Kitchener',
-    },
-    {
-        data: [
-            { key: 'somethgin', time: '12:34', name: 'Union 12:34 - Milton 13:33' },
-            { key: 'sometghin', time: '12:34', name: 'Union 12:34 - Milton 13:33' },
-        ],
-        routeColor: '#FF0000',
-        title: 'Milton',
-    },
-];
+// const SECTIONS = [
+//     {
+//         data: [
+//             { key: 'something', time: '12:34', name: 'Union 12:34 - Georgetown 13:33' },
+//             { key: 'somethign', time: '12:34', name: 'Union 12:34 - Georgetown 13:33' },
+//         ],
+//         routeColor: '#0000BB',
+//         title: 'Kitchener',
+//     },
+//     {
+//         data: [
+//             { key: 'somethgin', time: '12:34', name: 'Union 12:34 - Milton 13:33' },
+//             { key: 'sometghin', time: '12:34', name: 'Union 12:34 - Milton 13:33' },
+//         ],
+//         routeColor: '#FF0000',
+//         title: 'Milton',
+//     },
+// ];
 
 
 function renderHeader(data: Object): * {
@@ -38,22 +39,22 @@ function renderDeparture(data: Object): * {
     return <Departure data={data} />;
 }
 
-
 function DepartureList(): * {
     return (
         <View style={styles.departureList}>
             <Button
                 style={styles.reloadDepartures}
                 title="Reload Departures"
+                onPress={store.refresh}
             />
             <SectionList
                 renderItem={renderDeparture}
                 renderSectionHeader={renderHeader}
-                sections={SECTIONS}
+                sections={store.departureData}
             />
         </View>
     );
 }
 
 
-export default DepartureList;
+export default observer(DepartureList);
---
+++ src/store/schedule.js
@@ -0,0 +1,131 @@
+// @flow
+
+import { observable, action, computed } from 'mobx';
+
+// the url for fetching trips
+const url = 'https://transit.land/api/v1/schedule_stop_pairs?operator_onestop_id=o-dpz-gotransit' +
+            '&date=theDate&origin_departure_between=startTime,endTime&origin_onestop_id=' +
+            's-dpz839kujp-unionstation%3Cun';
+
+const parseScheduleStopPair = (pair): * => {
+    const date = pair.service_start_date;
+    const time = pair.origin_departure_time;
+    return {
+        route: pair.route_onestop_id,
+        key: pair.trip,
+        time: new Date(date + 'T' + time + 'Z'),
+        name: pair.trip_headsign,
+    };
+};
+
+const parseRoute = (route): object => {
+    return {
+        id: route.onestop_id,
+        color: '#' + route.tags.route_color,
+        name: route.tags.route_long_name,
+    };
+};
+
+class scheduleStore {
+    @observable departures = [];
+    @observable routes = new Map();
+    @observable loadingRoutes = false;
+    @observable loadingDepartures = false;
+    @observable currentTime = new Date();
+    @observable debug = []
+
+    @action.bound refreshRoutes(): null {
+        this.routes.clear();
+        this.loadingRoutes = true;
+        this.debug = [];
+        fetch('https://transit.land/api/v1/routes?origin_onestop_id=s-dpz839kujp-unionstation%3Cun&operated_by=o-dpz-gotransit&vehicle_type=rail')
+            .then((response): * =>{
+                return response.json();
+            })
+            .then((rJSON): * => rJSON.routes.map(parseRoute))
+            .then((routes): * => {
+                routes.forEach((route): null => { this.routes.set(route.id, route); });
+            })
+            .finally((): null => this.loadingRoutes = false);
+    }
+
+    @action.bound refreshDepartures(): *  {
+        this.loadingDepartures = true;
+        // assume the timezone is correct
+        const dateString = this.currentTime.toISOString().substr(0,10);
+        const startTimeString = this.currentTime.toTimeString().substr(0,8);
+        const endTimeString = new Date(this.currentTime.getTime() + 1000*60*30).toTimeString().substr(0,8);
+        const formattedUrl = url.replace(
+            'theDate', dateString
+        ).replace(
+            'startTime', startTimeString
+        ).replace(
+            'endTime', endTimeString
+        );
+        fetch(formattedUrl)
+            .then((response): * =>{
+                return response.json();
+            })
+            .then((rJSON): * =>{
+                return rJSON.schedule_stop_pairs.map(parseScheduleStopPair)
+            })
+            .then((pairs): * => {
+                this.departures = pairs.sort((first, second): * =>
+                    first.time.getTime() - second.time.getTime()
+                );
+            })
+            .finally((): null => { this.loadingDepartures = false; });
+    }
+
+    @action.bound refresh(): *  {
+        this.refreshDepartures();
+        this.refreshRoutes();
+    }
+
+    @computed get nextDeparture(): * {
+        if (this.departures.length) {
+            return this.departures[0];
+        }
+    }
+
+    @computed get millisUntilDeparture(): * {
+        if (!this.nextDeparture) {
+            return -1;
+        }
+        return Math.max(
+            this.nextDeparture.time.getTime() -
+            this.currentTime.getTime() +
+            (this.currentTime.getTimezoneOffset() * 60 * 1000), 0);
+    }
+
+    @computed get departureData(): object {
+        const results = [];
+        this.routes.forEach((route) => {
+            const tripsForRoute = this.departures
+                .filter((departure): boolean => departure.route === route.id);
+            tripsForRoute.forEach(
+                ((trip): null => {
+                    if (trip.time && trip.time.toLocaleTimeString) {
+                        trip.time = trip.time.toLocaleTimeString();
+                    }
+                })
+            );
+            results.push({
+                data: tripsForRoute,
+                routeColor: route.color,
+                title: route.name,
+            });
+        });
+        return results
+            .sort((first, second): number => first.title.localeCompare(second.title))
+            .filter((route): number => route.data.length);
+    }
+
+    @computed get loading(): * {
+        return this.loadingDepartures || this.loadingRoutes;
+    }
+
+    timeUpdater = setInterval(() => { this.currentTime = new Date(); }, 100);
+}
+
+export const store = new scheduleStore();
