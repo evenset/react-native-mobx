c1a6f5ef83eaa0d3d582c3d8b620581ff3375139
Christopher Antila <christophera@evenset.com>
2017-10-04 16:06:46 -0400

    Change: rename app to 'react-native-mobx'

--- index.android.js
+++ index.android.js
@@ -1,6 +1,8 @@
+// @flow
+
 import { AppRegistry } from 'react-native';
 
 import RNMobxStarter from './src/components';
 
 
-AppRegistry.registerComponent('rnMobxStarter', () => RNMobxStarter);
+AppRegistry.registerComponent('react-native-mobx', () => RNMobxStarter);
